ef550f368bef81f6a784bf50620aac2775674cb8
CCShoop <caelantennis@gmail.com>
2022-01-18 18:54:05 -0500

    new files

---
+++ piInterface.py
@@ -0,0 +1,36 @@
+'''Script to run on Main device, written by Cael Shoop.'''
+# This script establishes ssh connections to the Pis on the Roombas.
+
+import paramiko
+
+
+def piConnect(piID):
+    if piID == 0:
+        print('<<< FEATURE IN PROGRESS >>>')
+        ip = '192.168.1.4'
+        piUsername = 'pi'
+        piPassword = 'ModelIoT'
+        ssh = paramiko.SSHClient()
+
+        ssh.load_system_host_keys()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, username=piUsername, password=piPassword, look_for_keys=False)
+
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('cd Roomba && python3 roombaDirect.py -d forward -t 10')
+        output = ssh_stdout.readlines()
+        for line in output:
+            print(line)
+
+        return ssh
+
+    elif piID == 1:
+        #ssh into Pi1
+        print('<<< FEATURE PENDING >>>')
+
+    else:
+        print('Invalid Roomba ID. Exiting.')
+        exit()
+
+
+def piDisconnect(ssh):
+    ssh.close()
---
+++ roombaInterface.py
@@ -0,0 +1,2 @@
+'''This script is to be run on the Raspberry Pis integrated on the Roombas. Written by Cael Shoop.'''
+
