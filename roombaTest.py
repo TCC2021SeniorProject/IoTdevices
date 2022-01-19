3823138a65433bc75281a9d363b9fd8cf9dc7eae
CCShoop <caelantennis@gmail.com>
2022-01-18 19:26:01 -0500

    has test with create2

---
+++ roombaTest.py
@@ -0,0 +1,15 @@
+'''Test File for moving Roombas. Written by Cael Shoop.'''
+
+from pycreate2 import Create2
+import time
+
+
+port = '/dev/ttyS0' # ttyUSB0
+roomba = Create2(port)
+roomba.start()
+roomba.safe()
+roomba.drive_direct(100, 100)
+roomba.drive_stop()
+sensors = bot.get_sensors()
+print(sensors.battery_charge())
+roomba.close()
