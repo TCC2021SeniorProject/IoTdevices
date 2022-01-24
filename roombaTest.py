84c4d2dbff494f26b92c9d164482e88abf9be4c7
CCShoop <caelantennis@gmail.com>
2022-01-24 17:09:25 -0500

    auto commit

--- roombaTest.py
+++ roombaTest.py
@@ -5,13 +5,22 @@ import time
 
 
 port = '/dev/ttyS0' # ttyUSB0
+print('Port set to \'/dev/ttyS0\'')
 roomba = Create2(port)
+print('Roomba object created')
 roomba.start()
+print('Roomba OI started')
 roomba.safe()
+print('Roomba set to safe mode')
 sensors = roomba.get_sensors()
+if sensors:
+    print('Got Roomba info')
 print(sensors.battery_charge())
 if sensors.battery_charge() > 30000:
-    roomba.drive_direct(100, 100)
-    sleep(10)
+    roomba.drive_direct(-200, -200)
+    sleep(2)
+    roomba.drive_stop()
+    roomba.drive_direct(-500, 500)
+    sleep(3)
     roomba.drive_stop()
 roomba.close()
