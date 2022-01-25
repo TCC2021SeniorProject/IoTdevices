79f1ab4dde95c548cde24a8eba0313da9da4fbc1
CCShoop <caelantennis@gmail.com>
2022-01-25 17:51:24 -0500

    auto commit

--- piInterface.py
+++ piInterface.py
@@ -1,76 +1,87 @@
-'''Script to run on main device. Written by Cael Shoop.'''
-# This script establishes ssh connections from the control device to the Pis on the Roombas.
+'''This script establishes ssh connections from the control device to the Pis on the Roombas. Written by Cael Shoop.'''
 
 import paramiko
 
-# Create SSH connection to both Pis
+# Create SSH connection to both Pis/Roombas
 def piConnect():
     print('Creating SSH connections to Pi0 and Pi1...')
     ip0 = '192.168.1.4'
     pi0User = 'pi'
     pi0Pw = 'ModelIoT'
+    port0 = '/dev/ttyUSB0'
     ip1 = '192.168.1.36'
     pi1User = 'pi'
     pi1Pw = 'ModelIoT'
+    port1 = '/dev/ttyUSB0'
 
-    print('Creating SSH objects for Pi0...')
+    print('Creating SSH object for Pi0...')
     ssh0 = paramiko.SSHClient()
-    print('Creating SSH objects for Pi1...')
+    print('Success.\nCreating SSH object for Pi1...')
     ssh1 = paramiko.SSHClient()
-    print('Loading system host keys for Pi0...')
+    print('Success.\nLoading system host keys for Pi0...')
     ssh0.load_system_host_keys()
-    print('Loading system host keys for Pi1...')
+    print('Success.\nLoading system host keys for Pi1...')
     ssh1.load_system_host_keys()
-    print('Setting missing host key policy for Pi0...')
+    print('Success.\nSetting missing host key policy for Pi0...')
     ssh0.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print('Setting missing host key policy for Pi1...')
+    print('Success.\nSetting missing host key policy for Pi1...')
     ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print('Success.')
 
     try:
         print('Establishing SSH connection to Pi0...')
         ssh0.connect(ip0, username=pi0User, password=pi0Pw, look_for_keys=False)
-        print('Pi0 SSH connection established.\nMoving to IoTdevices directory...')
+        print('Success.\nMoving to IoTdevices directory...')
         ssh0.exec_command('~/cd Roomba/IoTdevices')
-        print('Pi0 SSH is now in IoTdevices directory.')
+        print('Success.\nOpening Python shell...')
+        ssh0.exec_command('python3 -i')
+        print('Success.\nImporting pycreate2...')
+        ssh0.exec_command('from pycreate2 import Create2')
+        print('Success.\nCreating Roomba object...')
+        ssh0.exec_command('roomba = Create2(\'' + port0 + '\')')
+        print('Success.\nStarting Roomba0 OI...')
+        ssh0.exec_command('roomba.start()')
+        print('Success.\nRoomba0 is ready for commands.')
     except:
         print('Connection to Pi0 Failed.')
 
     try:
         print('Establishing SSH connection to Pi0...')
         ssh1.connect(ip1, username=pi1User, password=pi1Pw, look_for_keys=False)
-        print('Pi1 SSH connection established.\nMoving to IoTdevices directory...')
+        print('Success.\nMoving to IoTdevices directory...')
         ssh1.exec_command('~/cd Roomba/IoTdevices')
-        print('Pi1 SSH is now in IoTdevices directory.')
+        print('Success.\nOpening Python shell...')
+        ssh1.exec_command('python3 -i')
+        print('Success.\nImporting pycreate2...')
+        ssh1.exec_command('from pycreate2 import Create2')
+        print('Success.\nCreating Roomba object...')
+        ssh1.exec_command('roomba = Create2(\'' + port1 + '\')')
+        print('Success.\nStarting Roomba1 OI...')
+        ssh1.exec_command('roomba.start()')
+        print('Success.\nRoomba1 is ready for commands.')
     except:
         print('Connection to Pi1 Failed.')
 
     return ssh0, ssh1
 
-# Send parameter commands to Pis
-def piSend(ssh0, ssh1, com):
+# Send parameter commands to a Pi/Roomba
+def piSend(ssh, com, piNum):
     try:
-        print(f'Sending command \'{com}\' to Pi0...')
-        ssh0.exec_command(com)
-        print('Command sent to Pi0.')
+        print(f'Sending command \'{com}\' to Pi{piNum}...')
+        ssh.exec_command(com)
+        print(f'Command sent to Pi{piNum}.')
     except:
-        print('Command failed to send to Pi0.')
+        print(f'Command failed to send to Pi{piNum}.')
 
-    try:
-        print(f'Sending command \'{com}\' to Pi1...')
-        ssh1.exec_command(com)
-        print('Command sent to Pi1.')
-    except:
-        print('Command failed to send to Pi1.')
-
-
-# Test function to test connection to Roombas
+# Test function to test connection to Pis/Roombas
 def piTest(ssh0, ssh1):
-    if ssh0:
-        print('Running roombaTest.py on Pi0...')
-        ssh0_stdin, ssh0_stdout, ssh0_stderr = ssh0.exec_command('python3 roombaTest.py')
-    if ssh1:
-        print('Running roombaTest.py on Pi1...')
-        ssh1_stdin, ssh1_stdout, ssh1_stderr = ssh1.exec_command('python3 roombaTest.py')
+    print('Running roombaTest.py on both devices...')
+    ssh0.exec_command('roomba.close()')
+    ssh1.exec_command('roomba.close()')
+    ssh0.exec_command('quit()')
+    ssh1.exec_command('quit()')
+    ssh0_stdin, ssh0_stdout, ssh0_stderr = ssh0.exec_command('python3 roombaTest.py')
+    ssh1_stdin, ssh1_stdout, ssh1_stderr = ssh1.exec_command('python3 roombaTest.py')
     
     if ssh0:
         print('Pi0 output:')
@@ -88,6 +99,8 @@ def piTest(ssh0, ssh1):
 def piDisconnect(ssh0, ssh1):
     try:
         print('Closing Pi0 SSH connection...')
+        ssh0.exec_command('roomba.close()')
+        ssh0.exec_command('quit()')
         ssh0.close()
         print('Successfully closed Pi0 SSH connection.')
     except:
@@ -95,6 +108,8 @@ def piDisconnect(ssh0, ssh1):
 
     try:
         print('Closing Pi1 SSH connection...')
+        ssh0.exec_command('roomba.close()')
+        ssh0.exec_command('quit()')
         ssh1.close()
         print('Successfully closed Pi1 SSH connection.')
     except:
---
+++ testssh.py
@@ -0,0 +1,16 @@
+'''Script to test piInterface.py. Written by Cael Shoop.'''
+
+import paramiko
+import piInterface as piI
+import time
+
+
+ssh0, ssh1 = piI.piConnect()
+sleep(1)
+piI.piSend(ssh0, 'drive_direct(-200, -200)', 0)
+piI.piSend(ssh1, 'drive_direct(-200, -200)', 1)
+time.sleep(2)
+piI.piSend(ssh0, 'drive_direct(-300, 300)', 0)
+piI.piSend(ssh1, 'drive_direct(300, -300)', 1)
+time.sleep(3)
+piI.piDisconnect(ssh0, ssh1)
